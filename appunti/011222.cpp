852ee0da37f1de76b8d65b6d618658b121fa3f78
Polliog <polliogiuseppe@live.it>
2022-12-01 15:58:23 +0100

    appunti

---
+++ appunti/011222.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+//void inizializza_vettore(int v[],int dim) {
+//    cout << "Inserisci i numeri";
+//    for (int i = 0; i < dim; i++) {
+//        cin >> v[i];
+//    }
+//}
+//
+//void stampa_vettore(int v[], int dim) {
+//    for (int i = 0; i < dim; i++) {
+//        cout << v[i];
+//    }
+//}
+//
+//int main() {
+//    int dim = 5;
+//    int v[dim];
+//
+//    inizializza_vettore(v,dim);
+//    stampa_vettore(v,dim);
+//}
+//
+////ricrea con matrici
+//
+//void inizializza_matrice(int m[][3], int dim) {
+//    cout << "Inserisci i numeri";
+//    for (int i = 0; i < dim; i++) {
+//        for (int j = 0; j < 3; j++) {
+//            cin >> m[i][j];
+//        }
+//    }
+//}
+//
+//void stampa_matrice(int m[][3], int dim) {
+//    for (int i = 0; i < dim; i++) {
+//        for (int j = 0; j < 3; j++) {
+//            cout << m[i][j];
+//        }
+//    }
+//}
+//
+//int main() {
+//    int dim = 5;
+//    int m[dim][3];
+//
+//    inizializza_matrice(m,dim);
+//    stampa_matrice(m,dim);
+//}
+
+//scrivere fattoriale in maniera iterativa e ricorsiva
