aaffeec0f75337ca1909aae02272b118e5ceafa7
Polliog <polliogiuseppe@live.it>
2022-12-07 14:57:29 +0100

    Esercizi svolti

--- E071222/esercizio_1.cpp
+++ E071222/esercizio_1.cpp
@@ -1,6 +1,35 @@
 //Triangolo di tartaglia
 
 #include <iostream>
+
 using namespace std;
 
 
+int main() {
+    int r = 5;
+
+    cout << "Inserisci un numero:" << endl;
+    //cin >> r;
+
+    int m[5][5];
+
+    m[0][0] = r;
+    for (int i = 1; i < r; i++) {
+        m[i][0] = r;
+        for (int j = 1; j < r;j++) {
+            m[i][j] = m[i-1][j-1] + m[i-1][j];
+        }
+    }
+
+    for (int i = 0; i < r;i++) {
+        {
+            for (int j = 1;j<r;j++)
+                cout << " ";
+
+            for (int j = 0;j <= i;j++) {
+                cout << m[i][j] << "";
+                cout << "\n";
+            }
+        }
+    }
+}
